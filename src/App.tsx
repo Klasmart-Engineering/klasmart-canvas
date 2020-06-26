8447c0bbc34e1f798c19c18b4578a271f89854f9
Andres Perez <andresp@bluetrailsoft.com>
2020-06-26 17:47:22 -0500

    Setup canvas configuration for React Hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from 'react';
+import './canvas.css';
 
 function App() {
+  const ref: any = useRef(null);
+
+  useEffect(() => {
+    const canvas = ref.current;
+    const ctx = canvas.getContext('2d');
+    ctx.font = '30px Arial';
+    ctx.fillText('Hello World', 220, 175);
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <canvas ref={ref} width="600px" height="350px" />
     </div>
   );
 }
