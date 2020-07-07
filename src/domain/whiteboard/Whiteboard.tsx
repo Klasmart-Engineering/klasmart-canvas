f86099e3790b73234b062209cd437e4ca8c16513
Andres Perez <andresp@bluetrailsoft.com>
2020-07-07 17:56:07 -0500

    Add canvas HTML tag to Whiteboard.tsx Add Whiteboard styling. Add TTF fonts

--- src/domain/whiteboard/Whiteboard.tsx
+++ src/domain/whiteboard/Whiteboard.tsx
@@ -1,7 +1,16 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
+import './whiteboard.css';
+import { WhiteboardProvider } from './WhiteboardContext';
 
 function Whiteboard() {
-  return <div>Hello Whiteboard</div>;
+  return (
+    <div className="container">
+      <canvas id="canvas" style={{ border: '1px solid' }} />
+      <div className="toolbar-container">
+        <WhiteboardProvider>{/*<Toolbar />*/}</WhiteboardProvider>
+      </div>
+    </div>
+  );
 }
 
 export default Whiteboard;
