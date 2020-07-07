f9c135eacd427f00c72a53e2a1296945eabf37d1
Andres Perez <andresp@bluetrailsoft.com>
2020-07-07 17:21:35 -0500

    Create useText.tsx hooks and tests

---
+++ src/domain/whiteboard/hooks/useText.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { renderHook, act } from '@testing-library/react-hooks';
+import { useText } from './useText';
+
+test('should set text from initial value', () => {
+  const { result } = renderHook(() => useText());
+  expect(result.current.text).toBe('');
+  expect(typeof result.current.updateText).toBe('function');
+});
+
+test('should update text value', () => {
+  const { result } = renderHook(() => useText());
+  act(() => {
+    result.current.updateText('I want to go to the movies');
+  });
+  expect(result.current.text).toBe('I want to go to the movies');
+});
