c6214ffebf9ca3db4df00207ec719075ac2c9ef5
Axel Wettervik <axel@calmid.com>
2020-07-22 13:04:29 +0900

    Add the PoC whiteboard (without network).

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
+import { Grid } from '@material-ui/core';
 import React from 'react';
-import Whiteboard from './domain/whiteboard/Whiteboard';
+import Toolbar from './poc/whiteboard/components/Toolbar';
+import { Whiteboard } from './poc/whiteboard/components/Whiteboard';
+import SharedEventSerializerContextProvider from './poc/whiteboard/context-provider/SharedEventSerializerProvider';
+import WhiteboardContextProvider from './poc/whiteboard/context-provider/WhiteboardContextProvider';
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <Whiteboard />
-      </div>
+      <Grid container direction="row" justify="center" spacing={1}>
+        <SharedEventSerializerContextProvider>
+          <WhiteboardContextProvider whiteboardId="demo" userId="test1">
+            <div style={{width: 400, height: 400}}>
+              <Whiteboard containerHeight={300} height={"100%"}/>
+              <Toolbar />
+            </div>
+          </WhiteboardContextProvider>
+          <WhiteboardContextProvider whiteboardId="demo" userId="test2">
+            <div style={{width: 400, height: 400}}>
+              <Whiteboard containerHeight={300} height={"100%"}/>
+              <Toolbar />
+            </div>
+          </WhiteboardContextProvider>
+          <WhiteboardContextProvider whiteboardId="demo" userId="test3">
+            <div style={{width: 400, height: 400}}>
+              <Whiteboard containerHeight={300} height={"100%"}/>
+              <Toolbar />
+            </div>
+          </WhiteboardContextProvider>
+        </SharedEventSerializerContextProvider>
+      </Grid>
     </div>
   );
 }
