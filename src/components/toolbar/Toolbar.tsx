b5cbb3bdf691c765964da01ad90625806703551e
Ismael Perez <ismaelp@bluetrailsoft.com>
2020-07-23 15:10:31 -0500

    Changes per prettier on previous fix.

--- src/components/toolbar/Toolbar.tsx
+++ src/components/toolbar/Toolbar.tsx
@@ -92,9 +92,13 @@ function Toolbar() {
       Add Shape Tool, and Add Text Tool the selected object will be deselected;
       Erase Type and Add Text cases will be handled in WhiteboardContext
     */
-   if (tool !== ELEMENTS.ERASE_TYPE_TOOL && tool !== ELEMENTS.ADD_TEXT_TOOL && tool !== ELEMENTS.ADD_SHAPE_TOOL) {
-    discardActiveObject();
-  }
+    if (
+      tool !== ELEMENTS.ERASE_TYPE_TOOL &&
+      tool !== ELEMENTS.ADD_TEXT_TOOL &&
+      tool !== ELEMENTS.ADD_SHAPE_TOOL
+    ) {
+      discardActiveObject();
+    }
 
     // set the clicked tool like active style in Toolbar
     setTools({
