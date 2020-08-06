acef994da430e61cd4013a03dddc45f79cc40d4a
Axel Wettervik <axel.wettervik@gmail.com>
2020-08-06 23:57:05 +0900

    Rebuild library.

--- lib/domain/whiteboard/WhiteboardCanvas.d.ts
+++ lib/domain/whiteboard/WhiteboardCanvas.d.ts
@@ -1,10 +1,19 @@
-import { FunctionComponent, ReactChild, ReactChildren } from 'react';
+import { CSSProperties, FunctionComponent, ReactChild, ReactChildren } from 'react';
+/**
+ * @field instanceId: Unique ID for this canvas. This enables fabricjs canvas to know which target to use.
+ * @field userId: The user's ID, events originating from this canvas will contain this ID.
+ * @field style: How the canvas should be styled.
+ * @field pointerEvents: Enable or disable pointer interaction.
+ * @field width: The width of this canvas.
+ * @field height: The height of this canvas.
+ * @field filterUsers: Only render remote events originating from userId's in this list.
+ */
 export declare type Props = {
     children?: ReactChild | ReactChildren | null | any;
     instanceId: string;
     userId: string;
+    style: CSSProperties;
     pointerEvents: boolean;
-    display: boolean;
     width?: string | number;
     height: string | number;
     filterUsers?: string[];
