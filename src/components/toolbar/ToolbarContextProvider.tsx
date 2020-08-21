ee8d587b870c60b568950a06c413461b9ec8b1ae
Axel Wettervik <axel@calmid.com>
2020-08-21 18:16:23 +0900

    Implement listing of tools, colors and styles.

--- src/components/toolbar/ToolbarContextProvider.tsx
+++ src/components/toolbar/ToolbarContextProvider.tsx
@@ -1,111 +1,144 @@
-import React, { createContext, ReactChild, ReactChildren, useCallback } from 'react';
+import React, {
+  createContext,
+  ReactChild,
+  ReactChildren,
+  useCallback,
+} from 'react';
+
+import IToolbarSelectorOption from '../../interfaces/toolbar/toolbar-selector/toolbar-selector-option';
+import IStyleOption from '../../interfaces/toolbar/toolbar-special-elements/style-option';
+import { colorPaletteOptions } from './toolbar-sections';
+import {
+  IThicknessStyle,
+  selectorOptionsWithId,
+  selectorStyleOptionsWithId,
+} from './toolbar-utils';
+
+export type ToolType =
+  | 'select'
+  | 'pointer'
+  | 'move'
+  | 'eraser'
+  | 'line'
+  | 'fill'
+  | 'text'
+  | 'shape'
+  | 'stamp';
+
+type OptionalToolOptions = IToolbarSelectorOption[] | undefined;
+
+const tools: Record<ToolType, IToolbarSelectorOption[] | undefined> = {
+  select: selectorOptionsWithId('pointers'),
+  pointer: selectorOptionsWithId('laser_pointer'),
+  move: selectorOptionsWithId('move_objects'),
+  eraser: selectorOptionsWithId('erase_type'),
+  line: selectorOptionsWithId('line_type'),
+  fill: selectorOptionsWithId('flood_fill'),
+  text: selectorOptionsWithId('add_text'),
+  shape: selectorOptionsWithId('add_shape'),
+  stamp: selectorOptionsWithId('add_stamp'),
+};
+
+const colors = colorPaletteOptions.reduce((map, color) => {
+  return map.set(color.id, color);
+}, new Map<string, IStyleOption>());
+
+const thickness = selectorStyleOptionsWithId('line_width')
+  .map((opt) => {
+    return { id: opt.id, style: opt.style, value: opt.value };
+  })
+  .reduce((map, style) => {
+    return map.set(style.id, style);
+  }, new Map<string, IThicknessStyle>());
 
 export interface IToolbarActions {
-    // TODO: Typed tool parameter.
-    selectTool: (tool: string) => void;
+  selectTool: (tool: ToolType, options?: IToolbarSelectorOption) => void;
 
-    // TODO: Typed color parameter.
-    selectColor: (color: string) => void;
+  selectColor: (color: IStyleOption) => void;
 
-    // TODO: Typed brushStyle input.
-    selectBrushStyle: (brushStyle: string) => void;
+  clear: (filter?: string) => void;
 
-    clear: (filter?: string) => void;
+  undo: () => void;
 
-    undo: () => void;
-
-    redo: () => void;
+  redo: () => void;
 }
 
 export interface IToolbarState {
-
+  tools: Record<ToolType, OptionalToolOptions>;
+  colors: Map<string, IStyleOption>;
+  thickness: Map<string, IThicknessStyle>;
 }
 
 export interface IToolbarStatus {
-
+  selectedTool: ToolType | undefined;
+  selectedColor: IStyleOption | undefined;
+  selectedToolOptions: IToolbarSelectorOption | undefined;
+  canRedo: boolean;
+  canUndo: boolean;
 }
 
 export interface IToolbarContext {
-    state: IToolbarState
-    status: IToolbarStatus
-    actions: IToolbarActions
+  state: IToolbarState;
+  status: IToolbarStatus;
+  actions: IToolbarActions;
 
-    // Data
-    // TODO: List of available tools.
-    // TODO: List of available colors.
-    // TODO: List of available text styles.
-    // TODO: List of available line styles.
-    // TODO: List of available shapes.
+  // Status
+  // TODO: Current selected tool
+  // TODO: Current selected color
+  // TODO: Current selected text style
+  // TODO: Current selected line style
+  // TODO: Current selected shape
+  // TODO: Undo/Redo available?
 
-    // Status
-    // TODO: Current selected tool
-    // TODO: Current selected color
-    // TODO: Current selected text style
-    // TODO: Current selected line style
-    // TODO: Current selected shape
-    // TODO: Undo/Redo available?
-
-    // Tools
-    // TODO: Function to select line tool.
-    // TODO: Function to select pen tool.
-    // TODO: Function to select text tool.
-    // TODO: Function to select shape tool.
-    // TODO: Function to select select/move tool.
-
-    // Styling
-    // TODO: Function to select text style (font/size).
-    // TODO: Function to select line (style/width).
-    // TODO: Function to select color.
-
-    // Actions
-    // TODO: Function to clear.
-    // TODO: Function to undo.
-    // TODO: Function to redo.
+  // Tools
+  // TODO: Function to select line tool.
+  // TODO: Function to select pen tool.
+  // TODO: Function to select text tool.
+  // TODO: Function to select shape tool.
+  // TODO: Function to select select/move tool.
+  // TODO: Function to select laster pointer tool.
 }
 
 export const Context = createContext<IToolbarContext | undefined>(undefined);
 
 export type Props = {
-    children?: ReactChild | ReactChildren | undefined
-}
+  children?: ReactChild | ReactChildren | undefined;
+};
 
-export default function ToolbarContextProvider({children}: Props): JSX.Element {
-    const selectToolAction = useCallback((tool: string) => {
+export default function ToolbarContextProvider({
+  children,
+}: Props): JSX.Element {
+  const selectToolAction = useCallback((_tool: ToolType, _options?: IToolbarSelectorOption) => {}, []);
 
-    }, []);
+  const selectColorAction = useCallback((_color: IStyleOption) => {}, []);
 
-    const selectColorAction = useCallback((color: string) => {
+  const clearAction = useCallback((_filter?: string) => {}, []);
 
-    }, []);
+  const undoAction = useCallback(() => {}, []);
 
-    const selectBrushStyleAction = useCallback((textStyle: string) => {
+  const redoAction = useCallback(() => {}, []);
 
-    }, []);
+  const actions: IToolbarActions = {
+    selectTool: selectToolAction,
+    selectColor: selectColorAction,
+    clear: clearAction,
+    undo: undoAction,
+    redo: redoAction,
+  };
+    
+    const status: IToolbarStatus = {
+        selectedTool: undefined,
+        selectedToolOptions: undefined,
+        selectedColor: undefined,
+        canRedo: false,
+        canUndo: false,
+    };
 
-    const clearAction = useCallback((filter?: string) => {
-
-    }, []);
-
-    const undoAction = useCallback(() => {
-
-    }, []);
-
-    const redoAction = useCallback(() => {
-
-    }, []);
-
-    const actions: IToolbarActions = {
-        selectTool: selectToolAction,
-        selectColor: selectColorAction,
-        selectBrushStyle: selectBrushStyleAction,
-        clear: clearAction,
-        undo: undoAction,
-        redo: redoAction,
-    }
-
-    return (
-        <Context.Provider value={{state: {}, status: {}, actions}}>
-            {children}
-        </Context.Provider>
-    )
+  return (
+    <Context.Provider
+      value={{ state: { tools, colors, thickness }, status, actions }}
+    >
+      {children}
+    </Context.Provider>
+  );
 }
