52d8737c14e86498791ddfd36278804bf0a7d728
Axel Wettervik <axel.wettervik@gmail.com>
2020-08-26 15:28:29 +0900

    Add more exports.

--- lib/exports.d.ts
+++ lib/exports.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="react" />
-import Toolbar from './components/toolbar/Toolbar';
+import { useToolbarContext } from './components/toolbar/toolbar-context-provider';
+import ToolbarExample from './components/toolbar/toolbar-example';
 declare const _default: {
     EventSerializerProvider: import("react").FunctionComponent<{
         children?: string | number | import("react").ReactElement<any, string | ((props: any) => import("react").ReactElement<any, string | any | (new (props: any) => import("react").Component<any, any, any>)> | null) | (new (props: any) => import("react").Component<any, any, any>)> | import("react").ReactChildren | Element[] | null | undefined;
@@ -9,6 +10,10 @@ declare const _default: {
         children: import("react").ReactNode;
         clearWhiteboardPermissions: import("./interfaces/canvas-events/clear-whiteboard-permissions").IClearWhiteboardPermissions;
     }) => JSX.Element;
-    Toolbar: typeof Toolbar;
+    Toolbar: {
+        useToolbarContext: typeof useToolbarContext;
+        ToolbarExample: typeof ToolbarExample;
+        Context: import("react").Context<import("./components/toolbar/toolbar-context-provider").IToolbarContext>;
+    };
 };
 export default _default;
