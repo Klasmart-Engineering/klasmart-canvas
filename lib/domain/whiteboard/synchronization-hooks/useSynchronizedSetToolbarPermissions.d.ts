4b5b24bd1932e0a8d486106250fc6f99b908fb4d
Axel Wettervik <axel.wettervik@gmail.com>
2020-09-01 15:19:08 +0900

    Rebuild lib after rebase.

--- lib/domain/whiteboard/synchronization-hooks/useSynchronizedSetToolbarPermissions.d.ts
+++ lib/domain/whiteboard/synchronization-hooks/useSynchronizedSetToolbarPermissions.d.ts
@@ -1,2 +1,2 @@
-declare const useSynchronizedSetToolbarPermissions: (userId: string, shouldHandleRemoteEvent: (id: string) => boolean, setToolbarIsEnabled: (enabled: boolean) => void) => void;
+declare const useSynchronizedSetToolbarPermissions: (canvas: fabric.Canvas | undefined, userId: string, shouldHandleRemoteEvent: (id: string) => boolean, setToolbarIsEnabled: (enabled: boolean) => void) => void;
 export default useSynchronizedSetToolbarPermissions;
