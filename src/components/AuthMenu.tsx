aafdfe4872a52c735eee0dd6f40470fb2aeedf38
Ismael Perez <ismaelp@bluetrailsoft.com>
2020-09-08 13:32:52 -0500

    Laster pointer permissions now functional.

--- src/components/AuthMenu.tsx
+++ src/components/AuthMenu.tsx
@@ -26,13 +26,20 @@ export default function AuthMenu(props: {
   const isTeacher = userId === 'teacher';
   const classes = useStyles();
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [anchorPointerEl, setAnchorPointerEl] = useState<null | HTMLElement>(null);
   const [selectedIndex, setSelectedIndex] = useState(1);
   const {
     state: { eventSerializer },
   } = useSharedEventSerializer();
 
+  const [pointerIndex, setPointerIndex] = useState(1);
+
   const handleClickListItem = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
+  };
+
+  const handleClickListPointerItem = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorPointerEl(event.currentTarget);
   };
 
   const handleMenuItemClick = (
@@ -41,7 +48,7 @@ export default function AuthMenu(props: {
   ) => {
     setSelectedIndex(index);
     setAnchorEl(null);
-    debugger;
+
     if (index === 1) {
       const payload = {
         id: userId,
@@ -58,6 +65,17 @@ export default function AuthMenu(props: {
       };
       eventSerializer?.push('setToolbarPermissions', payload);
     }
+  };
+
+  const handleMenuPointerClick = (
+    _event: React.MouseEvent<HTMLElement>,
+    index: number
+  ) => {
+    setPointerIndex(index);
+    setAnchorPointerEl(null);
+    
+    let payload = index === 1 ? { id: userId, target: { pointer: true }} : { id: userId, target: { pointer: false } };
+    eventSerializer?.push('setToolbarPermissions', payload);  
   };
 
   const handleClose = () => {
@@ -87,11 +105,6 @@ export default function AuthMenu(props: {
             primary="Authorize"
             secondary={options[selectedIndex]}
           />
-          <ListItemText
-            style={{ paddingLeft: '20px' }}
-            primary="Authorize Pointer"
-            secondary={pointerOptions[selectedIndex]}
-          />
           </ListItem>
         </List>
         <Menu
@@ -106,6 +119,40 @@ export default function AuthMenu(props: {
               key={option}
               selected={index === selectedIndex}
               onClick={(event) => handleMenuItemClick(event, index)}
+            >
+              {option}
+            </MenuItem>
+          ))}
+        </Menu>
+        
+
+        <List component="nav" aria-label="Device settings">
+          <ListItem
+            button
+            aria-haspopup="true"
+            aria-controls="lock-menu"
+            onClick={handleClickListPointerItem}
+          >
+          <ListItemText
+            style={{ paddingLeft: '20px' }}
+            primary="Authorize Pointer"
+            secondary={pointerOptions[pointerIndex]}
+          />
+          </ListItem>
+        </List>
+
+        <Menu
+          id="lock-menu-pointer"
+          anchorEl={anchorPointerEl}
+          keepMounted
+          open={Boolean(anchorPointerEl)}
+          onClose={handleClose}
+        >
+          {pointerOptions.map((option, index) => (
+            <MenuItem
+              key={option}
+              selected={index === selectedIndex}
+              onClick={(event) => handleMenuPointerClick(event, index)}
             >
               {option}
             </MenuItem>
