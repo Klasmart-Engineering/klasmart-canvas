d0a726d243d1c0305e351b12096e666c82944861
Andres Perez <andresp@bluetrailsoft.com>
2020-09-17 13:12:26 -0500

    Create checkbox menu Auth component.

--- src/components/AuthMenu.tsx
+++ src/components/AuthMenu.tsx
@@ -1,20 +1,38 @@
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
+import FormControl from '@material-ui/core/FormControl';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 import { useSharedEventSerializer } from '../domain/whiteboard/SharedEventSerializerProvider';
+import { WhiteboardContext } from '../domain/whiteboard/WhiteboardContext';
+import { IWhiteboardContext } from '../interfaces/whiteboard-context/whiteboard-context';
+
+// const useStyles = makeStyles((theme: Theme) =>
+//   createStyles({
+//     root: {
+//       backgroundColor: theme.palette.background.paper,
+//     },
+//   })
+// );
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      backgroundColor: theme.palette.background.paper,
+      display: 'flex',
+    },
+    formControl: {
+      margin: theme.spacing(3),
     },
   })
 );
 
+// const options = ['Revoke all Users', 'Authorize all Users'];
 const options = ['Revoke all Users', 'Authorize all Users'];
 const pointerOptions = [
   'Pointer: Revoke all users',
@@ -27,7 +45,7 @@ export default function AuthMenu(props: {
 }) {
   const { userId } = props;
   const isTeacher = userId === 'teacher';
-  const classes = useStyles();
+  // const classes = useStyles();
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const [anchorPointerEl, setAnchorPointerEl] = useState<null | HTMLElement>(
     null
@@ -90,6 +108,35 @@ export default function AuthMenu(props: {
     setAnchorEl(null);
   };
 
+  //***
+  const classes = useStyles();
+  const { serializerToolbarState } = useContext(
+    WhiteboardContext
+  ) as IWhiteboardContext;
+  const [localToolbarState, setLocalToolbarState] = useState(
+    serializerToolbarState
+  );
+  const { pointer, move, erase } = localToolbarState;
+
+  const handleToolbarChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLocalToolbarState({
+      ...localToolbarState,
+      [event.target.name]: event.target.checked,
+    });
+  };
+
+  useEffect(() => {
+    const payload = {
+      id: userId,
+      target: {
+        toolbarState: localToolbarState,
+      },
+    };
+
+    eventSerializer?.push('setToolbarPermissions', payload);
+  }, [userId, eventSerializer, localToolbarState]);
+  //***
+
   if (!isTeacher) {
     return null;
   }
@@ -102,36 +149,37 @@ export default function AuthMenu(props: {
         }}
         className={classes.root}
       >
-        <List component="nav" aria-label="Device settings">
-          <ListItem
-            button
-            aria-haspopup="true"
-            aria-controls="lock-menu"
-            onClick={handleClickListItem}
-          >
-            <ListItemText
-              primary="Authorize"
-              secondary={options[selectedIndex]}
-            />
-          </ListItem>
-        </List>
-        <Menu
-          id="lock-menu"
-          anchorEl={anchorEl}
-          keepMounted
-          open={Boolean(anchorEl)}
-          onClose={handleClose}
-        >
-          {options.map((option, index) => (
-            <MenuItem
-              key={option}
-              selected={index === selectedIndex}
-              onClick={(event) => handleMenuItemClick(event, index)}
-            >
-              {option}
-            </MenuItem>
-          ))}
-        </Menu>
+        {/*<List component="nav" aria-label="Device settings">*/}
+        {/*  <ListItem*/}
+        {/*    button*/}
+        {/*    aria-haspopup="true"*/}
+        {/*    aria-controls="lock-menu"*/}
+        {/*    onClick={handleClickListItem}*/}
+        {/*  >*/}
+        {/*    <ListItemText*/}
+        {/*      primary="Authorize"*/}
+        {/*      secondary={options[selectedIndex]}*/}
+        {/*    />*/}
+        {/*  </ListItem>*/}
+        {/*</List>*/}
+
+        {/*<Menu*/}
+        {/*  id="lock-menu"*/}
+        {/*  anchorEl={anchorEl}*/}
+        {/*  keepMounted*/}
+        {/*  open={Boolean(anchorEl)}*/}
+        {/*  onClose={handleClose}*/}
+        {/*>*/}
+        {/*  {options.map((option, index) => (*/}
+        {/*    <MenuItem*/}
+        {/*      key={option}*/}
+        {/*      selected={index === selectedIndex}*/}
+        {/*      onClick={(event) => handleMenuItemClick(event, index)}*/}
+        {/*    >*/}
+        {/*      {option}*/}
+        {/*    </MenuItem>*/}
+        {/*  ))}*/}
+        {/*</Menu>*/}
 
         <List component="nav" aria-label="Device settings">
           <ListItem
@@ -164,6 +212,73 @@ export default function AuthMenu(props: {
               {option}
             </MenuItem>
           ))}
+        </Menu>
+
+        <List component="nav" aria-label="Device settings">
+          <ListItem
+            button
+            aria-haspopup="true"
+            aria-controls="lock-menu-2"
+            onClick={handleClickListItem}
+          >
+            <ListItemText
+              primary="Authorize Tools"
+              // secondary={options[selectedIndex]}
+            />
+          </ListItem>
+        </List>
+
+        <Menu
+          id="lock-menu-2"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          {/*{options.map((option, index) => (*/}
+          {/*  <MenuItem*/}
+          {/*    key={option}*/}
+          {/*    selected={index === selectedIndex}*/}
+          {/*    onClick={(event) => handleMenuItemClick(event, index)}*/}
+          {/*  >*/}
+          {/*    {option}*/}
+          {/*  </MenuItem>*/}
+          {/*))}*/}
+
+          <FormControl component="fieldset" className={classes.formControl}>
+            <FormGroup>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={pointer}
+                    onChange={handleToolbarChange}
+                    name="pointer"
+                  />
+                }
+                label="Laser Pointer"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={move}
+                    onChange={handleToolbarChange}
+                    name="move"
+                  />
+                }
+                label="Move objects"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={erase}
+                    onChange={handleToolbarChange}
+                    name="erase"
+                  />
+                }
+                label="Erase objects"
+              />
+            </FormGroup>
+          </FormControl>
         </Menu>
       </div>
     </div>
