99ea45ca117846403cb66a7a4f7733a444a3655c
Ismael Perez <ismaelp@bluetrailsoft.com>
2020-09-23 09:43:42 -0500

    FIx for app crashing when clearing a whiteboard and undoing when there are no states.

--- src/domain/whiteboard/reducers/undo-redo.tsx
+++ src/domain/whiteboard/reducers/undo-redo.tsx
@@ -214,7 +214,8 @@ const reducer = (
     case SET: {
       if (
         !action.event ||
-        (action.event.type === 'removed' && state.activeStateIndex === null)
+        (action.event.type === 'removed' && state.activeStateIndex === null) ||
+        (!action.payload?.length && !state.states.length)
       ) {
         return state;
       }
