4d0d6fe2df13c7a0cee20d624587c1a079bb08d5
Marlon Torres <marlont@bluetrailsoft.com>
2020-10-07 20:20:21 -0500

    issue solved

--- src/domain/whiteboard/utils/findIntersectedObjects.ts
+++ src/domain/whiteboard/utils/findIntersectedObjects.ts
@@ -1,5 +1,10 @@
-import { TypedShape } from "../../../interfaces/shapes/shapes";
+import { TypedShape } from '../../../interfaces/shapes/shapes';
+import { isEmptyShape } from './shapes';
 
-export const findIntersectedObjects = (mainObject: TypedShape, objectsList: TypedShape[]) => (
-  objectsList.filter((o : TypedShape) => mainObject.intersectsWithObject(o))
-)
+export const findIntersectedObjects = (
+  mainObject: TypedShape,
+  objectsList: TypedShape[]
+) =>
+  objectsList.filter(
+    (o: TypedShape) => mainObject.intersectsWithObject(o) && !isEmptyShape(o)
+  );
