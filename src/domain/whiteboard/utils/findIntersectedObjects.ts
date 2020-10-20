4cdfc55966857778fc3348a829847ef8a3a92d60
Marlon Torres <marlont@bluetrailsoft.com>
2020-10-19 19:18:58 -0500

    corner case issue, advance

--- src/domain/whiteboard/utils/findIntersectedObjects.ts
+++ src/domain/whiteboard/utils/findIntersectedObjects.ts
@@ -10,6 +10,27 @@ export const findIntersectedObjects = (
     to could difference it in the mainObject color data */
   const differentStroke = '#ababab';
 
+  const validColors = [
+    '#ffffff',
+    '#e6e6e6',
+    '#808080',
+    '#000000',
+    '#f8433f',
+    '#5fe119',
+    '#347dfa',
+    '#44f9f9',
+    '#f289fe',
+    '#fbe739',
+    '#fb823f',
+    '#8880fc',
+    '#0c7cfa',
+  ];
+
+  let originalBackground: string;
+  originalBackground = mainObject.backgroundColor || 'transparent';
+  mainObject.set('backgroundColor', '#acacac');
+  canvas.renderAll();
+
   /**
    * Check if really the given object is intersecting the mainObject
    * @param {TypedShape} object - object to check
@@ -30,11 +51,14 @@ export const findIntersectedObjects = (
       } else {
         if (!object.stroke) return;
 
+        canvas.renderAll();
         let originalStroke = object.stroke;
 
+        showOtherObjects(false, object);
         object.set({
           stroke: differentStroke,
         });
+
         canvas.renderAll();
 
         let data = getMainObjectColorData();
@@ -67,21 +91,18 @@ export const findIntersectedObjects = (
    * @param {Uint8ClampedArray} colorData - color data to review
    * @param {TypedShape} currentObject - actual compared object
    */
-  const imageIsTouching = (
-    colorData: Uint8ClampedArray,
-    currentObject: TypedShape
-  ) => {
-    for (let i = 0; i < colorData.length; i += 4) {
+  const imageIsTouching = (colorData: ImageData, currentObject: TypedShape) => {
+    for (let i = 0; i < colorData.data.length; i += 4) {
       let currentColor = [
-        colorData[i],
-        colorData[i + 1],
-        colorData[i + 2],
-        colorData[i + 3],
+        colorData.data[i],
+        colorData.data[i + 1],
+        colorData.data[i + 2],
+        colorData.data[i + 3],
       ];
 
       if (
         rgbaDataToHexadecimalColor(currentColor) !== canvas.backgroundColor &&
-        colorData[i + 3] !== 0
+        colorData.data[i + 3] !== 0
       ) {
         showOtherObjects(true, currentObject);
         canvas.renderAll();
@@ -90,6 +111,7 @@ export const findIntersectedObjects = (
     }
 
     showOtherObjects(true, currentObject);
+    // mainObject.set('backgroundColor', originalBackground);
     canvas.renderAll();
     return false;
   };
@@ -103,31 +125,150 @@ export const findIntersectedObjects = (
    * for the currentObject
    */
   const commonObjectIsTouching = (
-    colorData: Uint8ClampedArray,
+    colorData: ImageData,
     currentObject: TypedShape,
     originalStroke: string
   ) => {
-    for (let i = 0; i < colorData.length; i += 4) {
+    console.log(originalStroke);
+    const pointIsNeighbor = (i: number) => {
+      let upLeftPoint =
+        i % colorData.width && colorData.data[i - colorData.width * 4 - 4] !== 0
+          ? rgbaDataToHexadecimalColor([
+              colorData.data[i - colorData.width * 4 - 1],
+              colorData.data[i - colorData.width * 4 - 2],
+              colorData.data[i - colorData.width * 4 - 3],
+              colorData.data[i - colorData.width * 4 - 4],
+            ])
+          : '#aN';
+
+      let upPoint =
+        colorData.data[i - colorData.width * 4 + 3] !== 0
+          ? rgbaDataToHexadecimalColor([
+              colorData.data[i - colorData.width * 4],
+              colorData.data[i - colorData.width * 4 + 1],
+              colorData.data[i - colorData.width * 4 + 2],
+              colorData.data[i - colorData.width * 4 + 3],
+            ])
+          : '#aN';
+
+      let upRightPoint =
+        i % colorData.width !== colorData.width - 1 &&
+        colorData.data[i - colorData.width * 4 + 7] !== 0
+          ? rgbaDataToHexadecimalColor([
+              colorData.data[i - colorData.width * 4 + 4],
+              colorData.data[i - colorData.width * 4 + 5],
+              colorData.data[i - colorData.width * 4 + 6],
+              colorData.data[i - colorData.width * 4 + 7],
+            ])
+          : '#aN';
+
+      let leftPoint =
+        i % colorData.width && colorData.data[i - 4] !== 0
+          ? rgbaDataToHexadecimalColor([
+              colorData.data[i - 1],
+              colorData.data[i - 2],
+              colorData.data[i - 3],
+              colorData.data[i - 4],
+            ])
+          : '#aN';
+
+      let rightPoint =
+        i % colorData.width !== colorData.width - 1 &&
+        colorData.data[i + 7] !== 0
+          ? rgbaDataToHexadecimalColor([
+              colorData.data[i + 4],
+              colorData.data[i + 5],
+              colorData.data[i + 6],
+              colorData.data[i + 7],
+            ])
+          : '#aN';
+
+      let downLeftPoint =
+        i % colorData.width && colorData.data[i + colorData.width * 4 - 4] !== 0
+          ? rgbaDataToHexadecimalColor([
+              colorData.data[i + colorData.width * 4 - 1],
+              colorData.data[i + colorData.width * 4 - 2],
+              colorData.data[i + colorData.width * 4 - 3],
+              colorData.data[i + colorData.width * 4 - 4],
+            ])
+          : '#aN';
+
+      let downPoint =
+        colorData.data[i + colorData.width * 4 + 3] !== 0
+          ? rgbaDataToHexadecimalColor([
+              colorData.data[i + colorData.width * 4],
+              colorData.data[i + colorData.width * 4 + 1],
+              colorData.data[i + colorData.width * 4 + 2],
+              colorData.data[i + colorData.width * 4 + 3],
+            ])
+          : '#aN';
+
+      let downRightPoint =
+        i % colorData.width !== colorData.width - 1 &&
+        colorData.data[i + colorData.width * 4 + 7] !== 0
+          ? rgbaDataToHexadecimalColor([
+              colorData.data[i + colorData.width * 4 + 4],
+              colorData.data[i + colorData.width * 4 + 5],
+              colorData.data[i + colorData.width * 4 + 6],
+              colorData.data[i + colorData.width * 4 + 7],
+            ])
+          : '#aN';
+
+      console.log(
+        upLeftPoint,
+        upPoint,
+        upRightPoint,
+        leftPoint,
+        rightPoint,
+        downLeftPoint,
+        downPoint,
+        downRightPoint
+      );
+
+      return (
+        validColors.includes(upLeftPoint) ||
+        validColors.includes(upPoint) ||
+        validColors.includes(upRightPoint) ||
+        validColors.includes(leftPoint) ||
+        validColors.includes(rightPoint) ||
+        validColors.includes(downLeftPoint) ||
+        validColors.includes(downPoint) ||
+        validColors.includes(downRightPoint)
+      );
+    };
+
+    for (let i = 0; i < colorData.data.length; i += 4) {
       let currentColor = [
-        colorData[i],
-        colorData[i + 1],
-        colorData[i + 2],
-        colorData[i + 3],
+        colorData.data[i],
+        colorData.data[i + 1],
+        colorData.data[i + 2],
+        colorData.data[i + 3],
       ];
 
-      if (rgbaDataToHexadecimalColor(currentColor) === differentStroke) {
+      if (
+        rgbaDataToHexadecimalColor(currentColor) === differentStroke &&
+        pointIsNeighbor(i)
+      ) {
+        showOtherObjects(true, currentObject);
         currentObject.set({
           stroke: originalStroke,
         });
+        // canvas.renderAll();
+
+        mainObject.set('backgroundColor', originalBackground);
         canvas.renderAll();
 
         return true;
       }
     }
 
+    showOtherObjects(true, currentObject);
     currentObject.set({
       stroke: originalStroke,
     });
+
+    mainObject.set('backgroundColor', originalBackground);
+    // canvas.renderAll();
     canvas.renderAll();
     return false;
   };
@@ -142,7 +283,7 @@ export const findIntersectedObjects = (
     objectsList.forEach((obj) => {
       if (mainObject.intersectsWithObject(obj) && currentObject !== obj) {
         obj.set({
-          opacity: status ? 1 : 0,
+          visible: status,
         });
       }
     });
@@ -152,14 +293,16 @@ export const findIntersectedObjects = (
    * Get the current mainObject color data
    */
   const getMainObjectColorData = () => {
-    return canvas
+    let currentData = canvas
       .getContext()
       .getImageData(
-        Number(mainObject.oCoords?.tl.x) - 10,
-        Number(mainObject.oCoords?.tl.y) - 10,
-        Number(mainObject.width) + 70,
-        Number(mainObject.height) + 70
-      ).data;
+        Number(mainObject.oCoords?.tl.x) - 1,
+        Number(mainObject.oCoords?.tl.y) - 1,
+        Number(mainObject.width) + 1,
+        Number(mainObject.height) + 1
+      );
+
+    return currentData;
   };
 
   return objectsList.filter((o: TypedShape) => isTouchingMainObject(o));
