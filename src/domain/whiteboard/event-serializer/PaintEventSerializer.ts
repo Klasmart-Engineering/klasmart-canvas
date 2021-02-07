66fb2ed86088d55955f8cd250073317ec16e6dc2
Marlon Torres <marlont@bluetrailsoft.com>
2021-02-07 14:20:08 -0600

    basePath added on stringified object

--- src/domain/whiteboard/event-serializer/PaintEventSerializer.ts
+++ src/domain/whiteboard/event-serializer/PaintEventSerializer.ts
@@ -4,6 +4,7 @@ import { ICanvasObject } from '../../../interfaces/objects/canvas-object';
 import { IToolbarUI } from '../../../interfaces/toolbar/toolbar-ui';
 import { IBrushSyncTarget } from '../../../interfaces/brushes/brush-sync-target';
 import { Image } from 'fabric/fabric-impl';
+import { IShapeCreationTarget } from '../../../interfaces/brushes/shape-creation-target';
 
 // TODO: This service should probably implement some sort of
 // event batching, especially the line drawing can generate
@@ -67,7 +68,8 @@ export type PayloadTarget =
     }
   | { type: string; svg: string }
   | IBrushSyncTarget
-  | string;
+  | string
+  | IShapeCreationTarget;
 export class PaintEventSerializer extends EventEmitter
   implements PaintEventSerializer {
   readonly multiplier: number;
@@ -109,6 +111,14 @@ export class PaintEventSerializer extends EventEmitter
     //
 
     // console.log(`Serializing event for object: ${object.id}`);
+
+    const shape = (object.target as IShapeCreationTarget).shape;
+
+    if (type === 'moving' && shape?.basePath) {
+      (object.target as IShapeCreationTarget).shape = shape.toJSON([
+        'basePath',
+      ]);
+    }
 
     const uniqueObjectId = object.id;
     const serialized: PainterEvent = {
