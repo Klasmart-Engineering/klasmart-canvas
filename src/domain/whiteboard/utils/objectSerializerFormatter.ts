0b431b4b6e7448071c13332eab97deddf547c014
Ismael Perez <ismaelp@bluetrailsoft.com>
2021-02-17 13:56:07 -0600

    Object cloning now fully functional. Permissions remain to be checked.

--- src/domain/whiteboard/utils/objectSerializerFormatter.ts
+++ src/domain/whiteboard/utils/objectSerializerFormatter.ts
@@ -1,92 +1,108 @@
 import { CANVAS_OBJECT_PROPS } from '../../../config/undo-redo-values';
-import { ICanvasBrush } from '../../../interfaces/brushes/canvas-brush';
-import { ICanvasPathBrush } from '../../../interfaces/brushes/canvas-path-brush';
-import { ICanvasShapeBrush } from '../../../interfaces/brushes/canvas-shape-brush';
-import { IUndoRedoEvent } from '../../../interfaces/canvas-events/undo-redo-event';
 import { ICanvasObject } from '../../../interfaces/objects/canvas-object';
-import { TypedShape } from '../../../interfaces/shapes/shapes';
 import { requiredEllipseProps, requiredPencilDashedProps, requiredProps } from '../canvas-actions/shapeProps';
 import { ObjectEvent, ObjectType } from '../event-serializer/PaintEventSerializer';
-import { SET } from '../reducers/undo-redo';
 
+/**
+ * Customizes payload for shape or object.
+ * @param shape Canvas shape or object
+ * @param target Custom props to emit
+ * @param type Shape type
+ * @param id Object Id.
+ */
+const formatShape = (shape: ICanvasObject, target: { [key: string]: any }, type: string | undefined, id: string | undefined) => {
+  requiredProps.forEach((prop: string) => {
+    if (shape && (shape as any)[prop]) {
+      target = { ...target, [prop]: (shape as any)[prop] };
+    }
+  });
+
+  return {
+    type,
+    target,
+    id,
+  };
+};
+
+/**
+ * Customizes payload for shape or object.
+ * @param shape Canvas shape or object
+ * @param target Custom props to emit
+ * @param type Shape type
+ * @param id Object Id.
+ */
+const formatEllipse = (shape: ICanvasObject, target: { [key: string]: any }, type: string | undefined, id: string | undefined) => {
+  requiredEllipseProps.forEach((prop: string) => {
+    if (shape && (shape as any)[prop]) {
+      target = { ...target, [prop]: (shape as any)[prop] };
+    }
+  });
+
+  return {
+    type,
+    target,
+    id,
+  };
+}
+
+/**
+ * Customizes payload for shape or object.
+ * @param shape Canvas shape or object
+ * @param target Custom props to emit
+ * @param type Shape type
+ * @param id Object Id.
+ */
+const formatNonStandardBrush = (shape: ICanvasObject, target: { [key: string]: any }, type: string | undefined, id: string | undefined) => {
+  requiredPencilDashedProps.forEach((prop: string) => {
+    if (shape && (shape as any)[prop]) {
+      target = { ...target, [prop]: (shape as any)[prop] };
+    }
+  });
+
+  return {
+    type,
+    target,
+    id,
+  };
+}
+
+/**
+ * Formats payload for a cloned object for real time features.
+ * @param shape Shape or object that is being cloned
+ * @param brushType Brush type used for object
+ * @param id Object id.
+ */
 export const objectSerializerFormatter = (shape: ICanvasObject, brushType: string, id?: string) => {
   const type: ObjectType = (shape.get('type') || 'path') as ObjectType;
 
   if (shape.shapeType === 'shape') {
+    let type = shape.type;
+    let target = {
+      type,
+      id: id ? id : shape.id,
+    };
     if (brushType !== 'pencil' && brushType !== 'dashed') {
-      let type = shape.type;
-      let payload = {};
-      let target = {
-        type,
-        id: id ? id : shape.id,
-      };
-
-      const requiredProps = requiredPencilDashedProps;
-
-      requiredProps.forEach((prop: string) => {
-        if (shape && (shape as any)[prop]) {
-          target = { ...target, [prop]: (shape as any)[prop] };
-        }
-      });
-
-      payload = {
-        type,
-        target,
-        id,
-      };
-
-      return payload;
+      return formatNonStandardBrush(shape, target, type, id);
     } else {
-      let type = shape.type;
-      let payload = {};
-      let target = {
-        type,
-        id,
-      };
-
       if (type !== 'ellipse') {
-        requiredProps.forEach((prop: string) => {
-          if (shape && (shape as any)[prop]) {
-            target = { ...target, [prop]: (shape as any)[prop] };
-          }
-        });
-
-        payload = {
-          type,
-          target,
-          id,
-        };
-
-        return payload;
+        return formatShape(shape, target, type, id);
       } else {
-        requiredEllipseProps.forEach((prop: string) => {
-          if (shape && (shape as any)[prop]) {
-            target = { ...target, [prop]: (shape as any)[prop] };
-          }
-        });
-
-        payload = {
-          type,
-          target,
-          id,
-        };
-
-        return payload;
+        return formatEllipse(shape, target, type, id);
       }
     }
-  } else  {
-      let target = shape;
+  } else {
+    let target = shape;
 
-      const payload: ObjectEvent = {
-        type,
-        target: {
-          ...target.toJSON(CANVAS_OBJECT_PROPS),
-          top: 0,
-          left: 0,
-        } as ICanvasObject,
-        id: id ? id : shape.id as string,
-      };
+    const payload: ObjectEvent = {
+      type,
+      target: {
+        ...target.toJSON(CANVAS_OBJECT_PROPS),
+        top: 0,
+        left: 0,
+      } as ICanvasObject,
+      id: id ? id : shape.id as string,
+    };
 
-      return payload;
+    return payload;
   }
 }
