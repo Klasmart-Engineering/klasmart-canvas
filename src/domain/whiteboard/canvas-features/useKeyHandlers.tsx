668b23eff0b4b5650e4797781dc4e4db75b3ba97
Marlon Torres <marlont@bluetrailsoft.com>
2021-03-04 11:39:44 -0600

    active is undefined issue solved

--- src/domain/whiteboard/canvas-features/useKeyHandlers.tsx
+++ src/domain/whiteboard/canvas-features/useKeyHandlers.tsx
@@ -50,7 +50,7 @@ export const useKeyHandlers = (
       const removeSelectedObjects = () => {
         let active = canvas.getActiveObject();
 
-        if ((active as fabric.IText).isEditing) return;
+        if ((active as fabric.IText)?.isEditing) return;
 
         canvas.discardActiveObject();
 
