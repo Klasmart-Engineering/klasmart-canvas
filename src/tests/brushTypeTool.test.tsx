3999b1ea61375efde87387ab04f4243655473604
Nicolas Carelli <nicolasc@bluetrailsoft.com>
2021-06-11 20:41:44 -0300

    tests added

--- src/tests/brushTypeTool.test.tsx
+++ src/tests/brushTypeTool.test.tsx
@@ -4,12 +4,6 @@ import {
   render,
   cleanup,
   fireEvent,
-  wait,
-  getByTestId,
-  getByTitle,
-  getByAltText,
-  getAllByText,
-  getAllByAltText,
   getAllByTitle,
 } from '@testing-library/react';
 import App from '../App';
@@ -104,5 +98,46 @@ describe('Brush Type Tool', () => {
     const objs2 = JSON.parse(localStorage.getItem('objects') as string);
     expect(objs2[objs2.length - 1].basePath.type).toBe('marker');
     
+  });
+
+  it(`should change te brush type of a free hand drawing`, async () => {
+    localStorage.removeItem('canvas:simulated:events');
+    const { container } = render(<App />);
+
+    const upperCanvas = container.getElementsByClassName(
+      'upper-canvas'
+    )[0] as HTMLCanvasElement;
+
+    const getObjBtn = document.getElementById(
+      'get-objects-button'
+    ) as HTMLButtonElement;
+
+    let arrowBtn = container.querySelector(
+      `[data-testid='toolbar-button-arrow-pencil_line']`
+    );
+    fireEvent.click(arrowBtn);
+
+    let brushType = getAllByTitle(container, `Pen Line`)[0];
+    fireEvent.click(brushType);
+
+    fireEvent.mouseDown(upperCanvas, { clientX: 60, clientY: 200 });
+    fireEvent.mouseMove(upperCanvas, { clientX: 100, clientY: 100 });
+    fireEvent.mouseMove(upperCanvas, { clientX: 200, clientY: 200 });
+    fireEvent.mouseMove(upperCanvas, { clientX: 60, clientY: 200 });
+    fireEvent.mouseUp(upperCanvas, { clientX: 60, clientY: 200 });
+
+    fireEvent.click(arrowBtn);
+    brushType = getAllByTitle(container, `Marker Line`)[0];
+    fireEvent.click(brushType);
+
+    // const selectorBtn = getAllByTitle(container, 'Move Objects')[0]
+    // fireEvent.click(selectorBtn)
+
+    fireEvent.mouseDown(upperCanvas, {clientX: 75, clientY: 150})
+    fireEvent.mouseUp(upperCanvas, {clientX: 75, clientY: 150})
+
+    fireEvent.click(getObjBtn);
+    const objs2 = JSON.parse(localStorage.getItem('objects') as string);
+    expect(objs2[objs2.length - 1].basePath.type).toBe('marker');
   });
 });
